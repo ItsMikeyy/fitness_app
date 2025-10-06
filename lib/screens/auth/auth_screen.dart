6bc08f59035ea329815b493d486bc2915b97bb46
ItsMikeyy <michaelcarey14@outlook.com>
2025-10-06 13:08:42 -0600

    account creation started

--- lib/screens/auth/auth_screen.dart
+++ lib/screens/auth/auth_screen.dart
@@ -1,12 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../../services/firebase_auth.dart';
 
-class AuthScreen extends StatelessWidget {
+class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
 
   @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  String _errorMessage = "";
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Column(children: [Text("Login"), Text("Signup")])),
+      body: SafeArea(
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: InputDecoration(labelText: "Email"),
+              controller: _emailController,
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: "Password"),
+              controller: _passwordController,
+            ),
+            Text(_errorMessage, style: TextStyle(color: Colors.red)),
+            ElevatedButton(
+              onPressed: () {
+                _createAccount();
+              },
+              child: Text("Create Account"),
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  void _createAccount() async {
+    try {
+      await AuthService().createAccount(
+        email: _emailController.text,
+        password: _passwordController.text,
+      );
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        _errorMessage = e.message ?? "An unknown error occurred";
+      });
+    }
   }
 }
--- lib/services/firebase_auth.dart
+++ lib/services/firebase_auth.dart
@@ -1,5 +1,37 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
+  User? get currentUser => auth.currentUser;
+
+  Stream<User?> get authStateChanges => auth.authStateChanges();
+
+  Future<UserCredential> signIn({
+    required String email,
+    required String password,
+  }) async {
+    return await auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<UserCredential> createAccount({
+    required String email,
+    required String password,
+  }) async {
+    return await auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<void> signOut() async {
+    await auth.signOut();
+  }
+
+  Future<void> sendPasswordResetEmail({required String email}) async {
+    await auth.sendPasswordResetEmail(email: email);
+  }
 }
